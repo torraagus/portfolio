ce5c9ccede073b431466e5782483104a6b9b2fea
Agustin Torra <torra.agustinignacio@gmail.com>
2020-07-28 07:56:56 -0300

    Create Main component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,55 @@ import "./App.css";
 import styled from "styled-components";
 import { NavBar } from "./components/navBar/NavBar";
 
+const Main = styled.div`
+	height: 72vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background: tomato;
+`;
+
+const Name = styled.h3`
+	margin: 0;
+	color: white;
+	letter-spacing: 5px;
+`;
+
+const Rol = styled.h1`
+	margin: 0;
+	color: white;
+	letter-spacing: 3px;
+	margin-bottom: 2rem;
+`;
+
+const Button = styled.a`
+	background-color: white;
+	color: tomato;
+	width: 200px;
+	height: 40px;
+	line-height: 40px;
+	border-radius: 15px;
+	font-weight: bold;
+	margin-top: .5rem;
+
+	:hover {
+		background-color: lime;
+		color: white;
+		cursor: pointer;
+	}
+`;
+
 function App() {
 	return (
 		<div className="App">
 			<NavBar />
+			<Main>
+				<Name>Agustin Ignacio Torra</Name>
+				<Rol>React web developer</Rol>
+				<Button>See my work</Button>
+				<Button>Resume</Button>
+			</Main>
 			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
