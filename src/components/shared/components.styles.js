538da4d7d3a6688f89b92cd8f75ade58e13fbb3b
Agustin Torra <torra.agustinignacio@gmail.com>
2020-07-29 17:04:33 -0300

    Create shared component Section

--- src/components/shared/components.styles.js
+++ src/components/shared/components.styles.js
@@ -1,7 +1,12 @@
-import styled from "styled-components"
+import styled from "styled-components";
+
+export const Section = styled.div`
+	padding-top: 12vh;
+	padding-bottom: 10vh;
+`;
 
 export const Title = styled.h1`
-	color: ${props => props.color ? props.color : "white"};
+	color: ${(props) => (props.color ? props.color : "white")};
 	text-transform: uppercase;
 	margin: 0;
 	padding: 0;
@@ -9,8 +14,8 @@ export const Title = styled.h1`
 `;
 
 export const Subtitle = styled.p`
-    color: #21e6c1;
-    font-weight: 600;
+	color: #21e6c1;
+	font-weight: 600;
 	margin: 0;
 	padding: 0;
 `;
