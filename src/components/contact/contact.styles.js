b295a178561734dd249a3ecd88586ca1e387fbae
Agustin Torra <torra.agustinignacio@gmail.com>
2020-07-30 17:13:33 -0300

    Update Contact section styles for responsiveness

--- src/components/contact/contact.styles.js
+++ src/components/contact/contact.styles.js
@@ -2,15 +2,32 @@ import styled from "styled-components";
 import { Section } from "../shared/components.styles";
 
 export const Contact = styled(Section)`
-	// min-height: 100vh;
 	background-color: #278ea5;
-`;
-
-export const Form = styled.form`
-	padding-top: 3rem;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+`;
+
+export const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	flex-flow: wrap;
+	justify-content: center;
+	padding-top: 3rem;
+	width: 60%;
+
+	@media (max-width: 1800px) {
+		width: 70%;
+	}
+	@media (max-width: 1024px) {
+		width: 80%;
+	}
+	@media (max-width: 768px) {
+		width: 90%;
+	}
+	@media (max-width: 512px) {
+		width: 100%;
+	}
 `;
 
 export const Label = styled.label`
@@ -19,33 +36,61 @@ export const Label = styled.label`
 	align-items: flex-start;
 	color: white;
 	font-weight: bold;
+	width: 35%;
+	margin-right: 0.5rem;
+
+	:nth-child(3) {
+		min-width: 70%;
+	}
+
+	@media (max-width: 1800px) {
+		margin-right: 0.5rem;
+		width: 35%;
+
+		:nth-child(3) {
+			min-width: 70%;
+		}
+	}
+
+	@media (max-width: 1024px) {
+		margin-right: 0.5rem;
+		width: 45%;
+
+		:nth-child(3) {
+			min-width: 90%;
+		}
+	}
+
+	@media (max-width: 512px) {
+		width: 100%;
+	}
 `;
 
 export const TextInput = styled.input`
 	height: 50px;
-	width: 300px;
+	width: 100%;
 	outline: none;
 	border-radius: 5px;
-	margin-top: 0.5rem;
-	margin-bottom: 1rem;
-	padding-left: 1rem;
+	margin: 0.5rem 0 1rem 0;
 	border: none;
+	text-indent: 0.5rem;
 `;
 
 export const TextArea = styled.textarea`
 	height: 200px;
-	width: 300px;
+	min-width: 100%;
 	outline: none;
 	border-radius: 5px;
 	margin-top: 0.5rem;
 	margin-bottom: 1rem;
-	padding: 1rem 0 0 1rem;
 	border: none;
+	text-indent: 0.5rem;
+	padding-top: 1rem;
 `;
 
 export const SendButton = styled.input`
 	height: 50px;
-	width: 320px;
+	width: 30%;
 	outline: none;
 	border: none;
 	border-radius: 5px;
@@ -59,5 +104,21 @@ export const SendButton = styled.input`
 	:hover {
 		cursor: pointer;
 		opacity: 90%;
+	}
+
+	@media (max-width: 1366) {
+		width: 40%;
+	}
+
+	@media (max-width: 1024px) {
+		width: 50%;
+	}
+
+	@media (max-width: 768px) {
+		width: 90%;
+	}
+
+	@media (max-width: 512px) {
+		width: 100%;
 	}
 `;
