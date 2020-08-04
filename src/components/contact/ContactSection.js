07728d08c9284a282d9828ededec68bc18a7761c
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-04 17:34:56 -0300

    Add Alert component and update Contact section

--- src/components/contact/ContactSection.js
+++ src/components/contact/ContactSection.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
 	Contact,
 	SendButton,
@@ -6,30 +6,81 @@ import {
 	Form,
 	Label,
 	TextArea,
+	Alert
 } from "./contact.styles";
 import { Title, Subtitle } from "../shared/components.styles";
 import Axios from "axios";
 
 export const ContactSection = () => {
+	const [error, setError] = useState(null);
+	const [sent, setSent] = useState(false);
+	const [sending, setSending] = useState(false);
 	const [subject, setSubject] = useState("");
 	const [from, setFrom] = useState("");
 	const [message, setMessage] = useState("");
 
+	const sendBtnRef = useRef(null);
+
 	const onSubmit = (e) => {
 		e.preventDefault();
+		if (subject.length == 0 || from.length == 0 || message.length == 0) {
+			setError("Please complete all the fields");
+			return;
+		}
+		setSending(true);
 		Axios.post("/send-email", {
 			from,
 			subject,
 			message,
 		})
-			.then((res) => console.log(res))
-			.catch((err) => console.log(err));
+			.then((res) => {
+				setSent(true);
+				setError(null);
+				console.log(res);
+			})
+			.catch((err) => {
+				setSent(false);
+				setError(`${err}`);
+				console.log(err);
+			}).finally(() => {
+				setSending(false);
+				setTimeout(() => {
+					resetForm();
+				}, 3000);
+			});
 	};
+
+	const resetForm = () => {
+		setSent(false);
+		setSending(false);
+		setError(null);
+		setFrom("");
+		setSubject("");
+		setMessage("");
+	};
+
+	useEffect(() => {
+		setError(null);
+	}, [from, message, subject]);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (sendBtnRef.current) {
+				if (sendBtnRef.current.value == "Sending . . .") {
+					sendBtnRef.current.value = "Sending";
+				}
+				sendBtnRef.current.value += " .";
+			}
+		}, 1000);
+		return () => clearInterval(interval);
+	}, []);
 
 	return (
 		<Contact id="contact">
 			<Title>Contact</Title>
 			<Subtitle>Use the following form to contact me</Subtitle>
+			{sent && <Alert type="success">Email enviado con exito </Alert>}
+			{error && <Alert type="error">{error}</Alert>}
 			<Form onSubmit={onSubmit}>
 				<Label>
 					Name
@@ -58,7 +109,10 @@ export const ContactSection = () => {
 						onChange={(e) => setMessage(e.target.value)}
 					/>
 				</Label>
-				<SendButton type="submit" value="Send" />
+				{!sending && <SendButton type="submit" value="Send" />}
+				{sending && (
+					<SendButton ref={sendBtnRef} type="button" value="Sending" />
+				)}
 			</Form>
 		</Contact>
 	);
