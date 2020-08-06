cc566b82253db4aaba5fe4542daec88fec89330a
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-05 22:47:19 -0300

    Add Modal component

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -13,24 +13,41 @@ import {
 	MenuLine,
 	BlackScreen,
 } from "./navBar.styles";
+import MyModal from "../modal/Modal";
 
 export const NavBar = () => {
 	const menuItems = [
 		{ name: "My work", path: "#my-work" },
-		{ name: "Resume", path: "/download" },
+		{ name: "Resume", path: "" },
 		{ name: "Github", path: "https://github.com/torraagus" },
 		{ name: "Linkedin", path: "https://www.linkedin.com/in/torraagustin" },
 		{ name: "About me", path: "#about-me" },
 		{ name: "Contact", path: "#contact" },
 	];
 	const [isOpen, setIsOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	};
 
 	const handleOnClick = (val) => {
 		setIsOpen(val);
 	};
 
+	const handleOnClickResume = () => {
+		setIsOpen(false);
+		setIsModalOpen(true);
+	};
+
 	return (
 		<>
+			<MyModal
+				title="Resume"
+				subtitle="Choose language..."
+				isOpen={isModalOpen}
+				onCloseModal={closeModal}
+			></MyModal>
 			<BlackScreen show={isOpen} onClick={() => handleOnClick(false)} />
 			<Nav>
 				<Logo>AIT</Logo>
@@ -38,16 +55,18 @@ export const NavBar = () => {
 					<Button href="#my-work" onClick={() => handleOnClick(false)}>
 						My work
 					</Button>
-					<ResumeBtn href="/download" onClick={() => handleOnClick(false)}>
-						Resume
-					</ResumeBtn>
+					<ResumeBtn onClick={handleOnClickResume}>Resume</ResumeBtn>
 				</NavButtons>
 				<Menu className={`${isOpen ? "menuActive" : ""}`}>
 					{menuItems.map((item) => (
 						<MenuItem key={item.name}>
-							<Item href={item.path} onClick={() => handleOnClick(false)}>
-								{item.name}
-							</Item>
+							{item.name == "Resume" ? (
+								<Item onClick={handleOnClickResume}>{item.name}</Item>
+							) : (
+								<Item href={item.path} onClick={() => handleOnClick(false)}>
+									{item.name}
+								</Item>
+							)}
 						</MenuItem>
 					))}
 				</Menu>
