49eac83260925429337eb6218343c5c029f5bf0d
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-07 23:14:15 -0300

    Add ResumeModal

--- src/components/main/MainSection.js
+++ src/components/main/MainSection.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { Main, Name, Rol, Button, ResumeBtn } from "./main.styles";
+import MyModal from "../shared/modal/Modal";
+import ResumeModal from "../resumeModal/ResumeModal";
+import useModal from "../../hooks/UseModal";
 
 export const MainSection = () => {
+	const { isModalOpen, closeModal, openModal } = useModal();
+
 	return (
-		<Main>
-			<Name>Agustin Ignacio Torra</Name>
-			<Rol>React web developer</Rol>
-			<Button href="#my-work">See my work</Button>
-			<ResumeBtn href="./download">Resume</ResumeBtn>
-		</Main>
+		<>
+			<MyModal isOpen={isModalOpen} onCloseModal={closeModal}>
+				<ResumeModal
+					title="Resume"
+					subtitle="Choose a language"
+					onCloseModal={closeModal}
+				/>
+			</MyModal>
+			<Main>
+				<Name>Agustin Ignacio Torra</Name>
+				<Rol>React web developer</Rol>
+				<Button href="#my-work">See my work</Button>
+				<ResumeBtn onClick={() => openModal()}>Resume</ResumeBtn>
+			</Main>
+		</>
 	);
 };
