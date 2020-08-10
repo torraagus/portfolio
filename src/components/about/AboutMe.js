0a8fec659be11f29af826b2d9a084d8cd580c5d7
Agustin Torra <torra.agustinignacio@gmail.com>
2020-08-10 17:58:56 -0300

    Get data of section from api/about

--- src/components/about/AboutMe.js
+++ src/components/about/AboutMe.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AboutMe, Description } from "./aboutMe.styles";
 import { Title, Subtitle } from "../shared/components.styles";
+import Axios from "axios";
 
 export const AboutMeSection = () => {
+	const [subtitle, setSubtitle] = useState("");
+	const [description, setDescription] = useState("");
+
+	useEffect(() => {
+		Axios.get("/api/about")
+			.then((res) => {
+				setSubtitle(res.data.subtitle);
+				setDescription(res.data.description);
+			})
+			.catch((err) => console.log(err));
+	}, []);
+
 	return (
 		<AboutMe id="about-me">
 			<Title color="#071e3d">About me</Title>
-			<Subtitle>Some things about myself</Subtitle>
-			<Description>
-				I am an enthusiastic of technology, a very creative person that pays
-				attention to detail and quality.
-			</Description>
+			<Subtitle>{subtitle}</Subtitle>
+			<Description>{description}</Description>
 		</AboutMe>
 	);
 };
