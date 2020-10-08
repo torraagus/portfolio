57fca83d7a2d96e40af475b49d7d1c65c78cf3dd
Agustin Torra <torra.agustinignacio@gmail.com>
2020-10-08 10:00:08 -0300

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-// import logo from './logo.svg';
 import "./App.css";
-import styled from "styled-components";
 import { NavBar } from "./components/navBar/NavBar";
 import { MainSection } from "./components/main/MainSection";
 import { WorkSection } from "./components/work/WorkSection";
@@ -18,20 +16,6 @@ function App() {
 			<AboutMeSection />
 			<ContactSection />
 			<ScrollArrow />
-			{/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
 		</div>
 	);
 }
