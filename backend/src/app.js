86e3efcf4bf4d4812443e48a53a0f0257b8af70b
Agustin Torra <torra.agustinignacio@gmail.com>
2020-10-08 10:01:11 -0300

    Remove token security to users route

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const path = require("path");
 const app = express();
 
-const { ensureToken, verifyToken } = require("./middlewares/jwt");
+// const { ensureToken, verifyToken } = require("./middlewares/jwt");
 
 // settings
 app.set("port", process.env.PORT || 4000);
@@ -14,12 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../..") + "/build"));
 
 // routes
-app.use(
-	"/api/users",
-	ensureToken,
-	verifyToken,
-	require("./routes/users.routes")
-);
+app.use("/api/users", require("./routes/users.routes"));
 app.use("/api/projects", require("./routes/projects.routes"));
 app.use("/api/about", require("./routes/about.routes"));
 app.use("/api/login", require("./routes/login.routes"));
